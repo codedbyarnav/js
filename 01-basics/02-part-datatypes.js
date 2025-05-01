ebebe22f9b4aa5b7203e3a4cc89fe7ec99883f03
codedbyarnav <arnavatri05@gmail.com>
2025-05-01 18:15:17 +0000

    02,03

---
+++ 01-basics/02-part-datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+//alert("hello") //we are using Node.js not browser
+console.log(3+
+    3
+);// code readability should be high
+
+let name = "Arnav"
+let age = "17"
+let isLogged = true
+
+// bigint
+// string => ""
+// boolean => true,false
+// null => standalone value
+// undefined => not been defined(it is diffrent from null)
+// symbol
+
+console.log(typeof age);
---
+++ 01-basics/03-part-conversionOperations.js
@@ -0,0 +1,30 @@
+let score = "78"
+console.log(typeof (score))
+
+let valueOfNumber = Number (score)
+
+console.log(typeof (valueOfNumber))
+console.log(valueOfNumber)
+
+// "45" => 45
+// "45abc" => NaN
+// true => 1 , false => 0
+
+let isLogged = "1"
+
+let IsBooleanLocked = Boolean(isLogged)
+
+console.log(isLogged);
+console.log(IsBooleanLocked);
+console.log(typeof IsBooleanLocked);
+
+// 1 => true , 0 => false 
+// "string" => true
+// "" => false
+
+let onlyString = 78
+let isString = String(onlyString)
+
+console.log(onlyString);
+console.log(isString);
+console.log(typeof isString);
