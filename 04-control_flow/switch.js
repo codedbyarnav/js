1cfb3c34475afe6f8118862098b4a42dc2e169fa
codedbyarnav <arnavatri05@gmail.com>
2025-05-12 20:13:21 +0000

    saving

---
+++ 04-control_flow/switch.js
@@ -0,0 +1,27 @@
+// switch statement
+ const months = 3
+
+ switch (months) {
+    case 1:
+        console.log("january");
+        
+        break;
+    case 2:
+        console.log("February");
+        
+        break;
+    case 3:
+        console.log("March");
+        
+        break;
+    case 4:
+        console.log("April");
+        
+        break;
+
+ 
+    default:
+        console.log("Month not found");
+        
+        break;
+ }
