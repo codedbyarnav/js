14130fd71e870ccc37816370d90e6890a53e39c5
codedbyarnav <arnavatri05@gmail.com>
2025-05-12 18:21:43 +0000

    iife

---
+++ 03-basics/04-iife.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expressions (IIFE)
+// it is used to remove pollution in global scope
+// end the iife using ;
+(function code(){
+    // named function
+    console.log(`Enter`);
+    
+})()
+
+(()=>{
+    console.log(`enter the value ${name}`);
+    
+})()
